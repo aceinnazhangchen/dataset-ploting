802ced38c8ac496374789f77724768890ef69015
zhangchen <czhang@aceinna.com>
2020-01-19 12:43:59 +0800

    fixed sort

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 
 const keysort = function(key,sortType){
     return function(a,b){
-        return sortType ?~~(a[key]-b[key]):~~(a[key]-b[key])
+        return sortType ?(a[key]-b[key]):(b[key]-a[key]);
     }
 }
 module.exports = {
