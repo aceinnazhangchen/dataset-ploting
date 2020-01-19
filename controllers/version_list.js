2d4933cb698630c696ec6efffd1a8c4a29de0312
zhangchen <czhang@aceinna.com>
2020-01-19 12:31:35 +0800

    sort modify

--- controllers/version_list.js
+++ controllers/version_list.js
@@ -7,19 +7,19 @@ var fn_version_list = async (ctx, next) => {
     var pathName = path.join(appRoot,"date");
     console.log(pathName);
     let {err,data} = await file_sys.readDir(pathName);
-    var file_list = [];
-    for(let i in data){
-        //let info = fs.stat(path.join(pathName,file));
-        console.log("version",path.join(pathName,data[i]));
-        let info = await file_sys.fileStat(path.join(pathName,data[i]));
-        //console.log(info);
-        file_list.push({name:data[i],timeSec:info.birthtimeMs,time:moment(info.birthtimeMs).format('YYYY-MM-DD HH:mm:ss')});
-    }
-    var list = file_list.sort(utils.keysort('timeSec',false));
     if(err){
         let msg = "search dir "+ pathName + " error !";
         await ctx.render('error.html', {title: 'error',msg});
     }else{
+        var list = [];
+        for(let i in data){
+            //let info = fs.stat(path.join(pathName,file));
+            console.log("version",path.join(pathName,data[i]));
+            let info = await file_sys.fileStat(path.join(pathName,data[i]));
+            //console.log(info);
+            list.push({name:data[i],timeSec:parseInt(info.birthtimeMs),time:moment(info.birthtimeMs).format('YYYY-MM-DD HH:mm:ss')});
+        }
+        list = list.sort(utils.keysort('timeSec',false));
         await ctx.render('version_list.html', {title: 'Version',list});
     }
 }
