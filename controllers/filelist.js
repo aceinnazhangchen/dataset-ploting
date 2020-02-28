96f870064fb0bf6eb602ca77f535963f54459258
zhangchen <czhang@aceinna.com>
2020-02-28 13:16:05 +0800

    Show file size

--- controllers/filelist.js
+++ controllers/filelist.js
@@ -2,6 +2,15 @@ var path = require('path');
 const file_sys = require('../utils/file_sys');
 const config = require('../config.json');
 
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
 var fn_file_list = async (ctx, next) => {
     var set = ctx.query.set;
     var setPath = "";
@@ -25,7 +34,7 @@ var fn_file_list = async (ctx, next) => {
         for(let i in data){
             let info = await file_sys.fileStat(path.join(pathName,data[i]));
             if(info.isDirectory() == false && path.extname(data[i]) != ".dif") continue;
-            list.push({name:data[i],dir:info.isDirectory()});
+            list.push({name:data[i],dir:info.isDirectory(),size:formatBytes(info.size)});
         }
         await ctx.render('fle_list.html', {title: 'List',set,version,parent,list});
     }
